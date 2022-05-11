b059ec15ffe51308065fd56b5dfec34a56357abb
guidojw <35309288+guidojw@users.noreply.github.com>
2022-05-11 23:05:26 +0200

    refactor: specify class_name in resources

--- app/resources/v1/debit/collection_resource.rb
+++ app/resources/v1/debit/collection_resource.rb
@@ -3,7 +3,7 @@ class V1::Debit::CollectionResource < V1::ApplicationResource
   model_name 'Debit::Collection'
   model_hint model: User, resource: V1::UserResource
 
-  has_one :author,  always_include_linkage_data: true
+  has_one :author, class_name: 'User', always_include_linkage_data: true
   has_many :transactions
 
   def fetchable_fields
